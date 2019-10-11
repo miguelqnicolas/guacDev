6bcd7023426167d59f4244c67de9fca2535f7209
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-10-10 23:51:44 -0400

    fixed sizing and spacing, added description

--- main.js
+++ main.js
@@ -25,15 +25,15 @@ function remove(e) {
 
 function check(stat) {
     toCount = stat.value;
-    if (stat.id == "start") {
-        display("stop");
-        remove("start");
-    } else if (stat.id == "stop") {
-        display("continue");
-        remove("stop");
+    if (stat.id == 'start') {
+        display('stop');
+        remove('start');
+    } else if (stat.id === 'stop') {
+        display('continue');
+        remove('stop');
     } else {
-        display("stop");
-        remove("continue");
+        display('stop');
+        remove('continue');
     }
 }
 
@@ -44,15 +44,15 @@ function count() {
             remseconds = seconds % 60;
             minutes = Math.floor(seconds / 60);
             if (minutes < 10) {
-                minutes = "0" + minutes;
+                minutes = '0' + minutes;
             }
             if (remseconds < 10) {
-                remseconds = "0" + remseconds;
+                remseconds = '0' + remseconds;
             }
-            container.innerHTML = minutes + ":" + remseconds;
+            container.innerHTML = minutes + 'm' + remseconds + 's';
         } else {
             // container.innerHTML = "Time's up!";
-            buttonsDiv.style.opacity = "0";
+            buttonsDiv.style.opacity = '0';
         }
     }
 }
@@ -60,12 +60,20 @@ function count() {
 function counting() {
     remseconds = seconds % 60;
     minutes = Math.floor(seconds / 60);
+    
+    // if (hours < 10) {
+    //     hours =  '0' + hours;
+    // }
+    
     if (minutes < 10) {
         minutes = "0" + minutes;
     }
     if (remseconds < 10) {
         remseconds = "0" + remseconds;
     }
-    container.innerHTML = minutes + ":" + remseconds;
+    container.innerHTML = minutes + "m" + remseconds + "s";
+
+    // hours + 'h' +
+
     setInterval(count, 1000);
 }
