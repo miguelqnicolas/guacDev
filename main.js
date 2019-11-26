054ac89d16b3b8915ea41d1642620b2a72762a8b
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-11-25 22:31:53 -0500

    cleaned up code, updated syntax, and made minor changes to styling

--- main.js
+++ main.js
@@ -1,79 +1,80 @@
-const container = document.querySelector('.counter');
-const buttonsDiv = document.querySelector('.buttons');
-const secInput = document.getElementById('seconds');
+const counter = document.querySelector('.counter');
+const secInput = document.querySelector('#seconds');
 
 let seconds;
 let remseconds;
 let minutes;
 let toCount = false;
 
-function toSubmit() {
-    display('start');
+// taking input
+const toSubmit = () => {
+    seconds = secInput.value;
     remove('seconds');
     remove('ok');
-    seconds = Number(secInput.value);
+    display('start');
     counting();
-}
+};
 
-function display(item) {
-    document.getElementById(item).style.display = 'block';
-}
+// button's id is passed through, and that button is now showing
+const display = item => {
+    document.querySelector(`#${item}`).style.display = 'block';
+};
 
-function remove(item) {
-    document.getElementById(item).style.display = 'none';
-}
+// button's id passed through, and now that button is removed
+const remove = item => {
+    document.querySelector(`#${item}`).style.display = 'none';
+};
 
-function check(stat) {
-    toCount = stat.value;
-    if (stat.id == 'start') {
+// storing input value in toCount and displaying buttons as need be depending on the id of the input value
+const check = item => {
+    toCount = item.value;
+    
+    if (item.id === 'start') {
         display('stop');
         remove('start');
-    } else if (stat.id === 'stop') {
+    } else if (item.id === 'stop') {
         display('continue');
         remove('stop');
     } else {
         display('stop');
         remove('continue');
-    }
-}
+    };
+};
 
-function count() {
+// function that actually counts
+const count = () => {
     if (seconds > 0) {
         if(toCount == true) {
             seconds--;
             remseconds = seconds % 60;
             minutes = Math.floor(seconds / 60);
+            
             if (minutes < 10) {
                 minutes = '0' + minutes;
             }
+            
             if (remseconds < 10) {
                 remseconds = '0' + remseconds;
             }
-            container.innerHTML = minutes + 'm' + remseconds + 's';
-        } else {
-            // container.innerHTML = "Time's up!";
-            buttonsDiv.style.opacity = '0';
-        }
-    }
-}
+            
+            counter.textContent = minutes + 'm' + remseconds + 's';
+        };
+    };
+};
 
-function counting() {
+// parses input and calls count function
+const counting = () => {
     remseconds = seconds % 60;
     minutes = Math.floor(seconds / 60);
     
-    // if (hours < 10) {
-    //     hours =  '0' + hours;
-    // }
-    
     if (minutes < 10) {
-        minutes = "0" + minutes;
+        minutes = '0' + minutes;
     }
     if (remseconds < 10) {
-        remseconds = "0" + remseconds;
+        remseconds = '0' + remseconds;
     }
-    container.innerHTML = minutes + "m" + remseconds + "s";
-
-    // hours + 'h' +
+    
+    counter.textContent = minutes + 'm' + remseconds + 's';
 
     setInterval(count, 1000);
-}
+};
