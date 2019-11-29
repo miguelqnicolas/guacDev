a5c3e347d464208cc0654a025683e69a0373310a
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-11-29 13:00:34 -0500

    netlify build folder test

---
+++ build/precache-manifest.1f751097b9d2325782c65161fff14c57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe322ea7598d0b771600777dc152b9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "996f06e7c63d7100ac93",
+    "url": "/static/css/main.c899436e.chunk.css"
+  },
+  {
+    "revision": "275a9cd351e9e7e11e3f",
+    "url": "/static/js/2.3d9806eb.chunk.js"
+  },
+  {
+    "revision": "996f06e7c63d7100ac93",
+    "url": "/static/js/main.30501bec.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  }
+]);
