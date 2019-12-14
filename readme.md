561d6aac95908fe10502f1a9b3a58b59f9b6d18f
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-12-14 00:15:29 -0500

    migrated, updated readme, and modified api call

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-## guacSearch
-A search app that allows you to search for an item on multiple marketplaces.
-Deployed at [search.guacdev.com](https://search.guacdev.com).
+## guacMarket
+A stock market research application
+Deployed at [market.guacdev.com](https://market.guacdev.com).
--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
+import Header from './components/Header';
+import Landing from './components/Landing';
+import Search from './components/Search';
+import Footer from './components/Footer';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 const App = () => {
-	const [input, setInput] = useState('');
-
-	const onChange = event => {
-		setInput(event.target.value);
-		console.log(input);
-	};
-
-	const onSubmit = event => {
-		event.preventDefault();
-		window.open(`https://www.facebook.com/marketplace/105807299459883/search/?query=${input}&vertical=C2C&sort=BEST_MATCH`);
-		window.open(`https://cnj.craigslist.org/search/sss?query=${input}&sort=rel`);
-		window.open(`https://www.letgo.com/en-us?searchTerm=${input}`);
-		window.open(`https://offerup.com/search/?q=${input}`);
-		window.open(`https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1312.R4.TR0.TRC0.A0.H0.TRS5&_nkw=${input}&_sacat=0`);
-		window.open(`https://www.mercari.com/search/?keyword=${input}`);
-		setInput('');
-	}
-
 	return (
-		<div className='App'>
-			<h1>guacSearch</h1>
-			<form onSubmit={onSubmit} autoComplete='off' spellCheck='false'>
-				<input type='text' value={input} onChange={onChange}/>
-				<button type='submit'><i className="fas fa-search"></i></button>
-			</form>
-			<p>Search for an item on multiple marketplaces in one go!</p>
-		</div>
+		<Router>
+			<div>
+				<Route path='/' component={Header}/>
+				<Route exact path='/' component={Landing}/>
+				<Route path='/search' component={Search}/>
+
+				<Footer/>
+			</div>
+		</Router>
 	);
 };
 
